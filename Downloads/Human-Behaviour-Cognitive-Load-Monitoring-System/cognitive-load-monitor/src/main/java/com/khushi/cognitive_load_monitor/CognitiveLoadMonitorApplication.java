fb316bef4f1b79ddfee2a2ebaaa7b77aedce9834
Khushi Sawalkar <khushi27sawalkar@gmail.com>
2026-03-14 00:07:10 +0530

    Add Spring Boot backend structure

---
+++ Downloads/Human-Behaviour-Cognitive-Load-Monitoring-System/cognitive-load-monitor/src/main/java/com/khushi/cognitive_load_monitor/CognitiveLoadMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.khushi.cognitive_load_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognitiveLoadMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognitiveLoadMonitorApplication.class, args);
+	}
+
+}
